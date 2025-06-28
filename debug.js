c1a313e6623a0bd16c431ffb626b8b17854172b3
Bimpss <bimpmeister@gmail.com>
2025-06-28 18:23:19 -0400

    Debug

---
+++ debug.js
@@ -0,0 +1,2 @@
+console.log("TELEGRAM_BOT_TOKEN length:", process.env.TELEGRAM_BOT_TOKEN?.length || "NOT SET");
+console.log("TELEGRAM_CHAT_ID length:", process.env.TELEGRAM_CHAT_ID?.length || "NOT SET");
